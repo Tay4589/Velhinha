53da99835fed5d54e65fadf90ae0525d14bf789d
Tay4589 <127732511+Tay4589@users.noreply.github.com>
2023-10-02 07:36:41 -0300

    Add files via upload

---
+++ AAAAAAAAAAAAA-main/AAAAAAAAAAAAA-main/Tic-Tac-Toe-main/Tic_Tac_Toe/js/main.js
@@ -0,0 +1,30 @@
+// Declaração de variáveis
+//var numero = 1;
+//var decimal = 1.5;
+//var texto = '';
+
+//let nome = '';
+//const pi = 3.1415;
+//console.log(nome);
+//console.log("Hoje não");
+
+// Declarar uma variável para representar os elementos do jogo
+let letra = "X"; // X ou O
+
+
+function joga(casa) {
+    let celulaClicada = document.getElementById(casa).innerHTML;
+
+    if(celulaClicada == 'X' || celulaClicada == 'O' ) {
+         alert("Está célula já tem um valor");
+    } else {
+        document.getElementById(casa).innerHTML = letra;
+
+    } if(letra== 'X') {
+        letra = 'O';
+    } else {
+        letra = 'X';
+    }
+
+
+}
